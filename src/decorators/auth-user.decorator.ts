542e29a0af7afd4b1a0f31c03f024a0ddd5fc40e
Changyuan Yang <qa894178522@qq.com>
2022-10-14 23:47:02 +0800

    feat: add AuthUser decorator note

--- src/decorators/auth-user.decorator.ts
+++ src/decorators/auth-user.decorator.ts
@@ -2,6 +2,9 @@ import type { Request } from 'express';
 
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
+/**
+ * 快速获取已通过授权的用户信息，而非手动通过Request获取
+ */
 export const AuthUser = createParamDecorator(
   (key: string | undefined, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest<Request>();
