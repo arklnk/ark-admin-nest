9dad7c9820e36a6a2a5d129528b3e555426c0cb9
Changyuan Yang <qa894178522@qq.com>
2022-10-14 21:02:42 +0800

    chore: update eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,15 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+
+    // custom
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^ignore',
+        destructuredArrayIgnorePattern: '^_',
+      },
+    ],
   },
 };
