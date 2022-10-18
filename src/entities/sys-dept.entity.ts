ad5cb36ff2b97eb175a4f6db08b607e6402cbb18
Changyuan Yang <qa894178522@qq.com>
2022-10-18 13:47:43 +0800

    feat: add SysDept entity

---
+++ src/entities/sys-dept.entity.ts
@@ -0,0 +1,56 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { AbstractEntity } from '/@/common/abstract.entity';
+
+@Entity({ name: 'sys_dept' })
+export class SysDept extends AbstractEntity {
+  @PrimaryGeneratedColumn({ unsigned: true })
+  id: number;
+
+  @Column({ name: 'parent_id', type: 'int', unsigned: true, comment: '父级id' })
+  parentId: number;
+
+  @Column({ type: 'varchar', length: 50, comment: '部门简称' })
+  name: string;
+
+  @Column({ type: 'varchar', length: 50, comment: '部门全称' })
+  fullName: string;
+
+  @Column({
+    name: 'unique_key',
+    type: 'varchar',
+    length: 50,
+    unique: true,
+    comment: '唯一标识',
+  })
+  uniqueKey: string;
+
+  @Column({
+    type: 'tinyint',
+    width: 1,
+    unsigned: true,
+    default: 1,
+    comment: '类型: 1=公司 2=子公司 3=部门	',
+  })
+  type: number;
+
+  @Column({
+    type: 'tinyint',
+    width: 1,
+    unsigned: true,
+    default: 1,
+    comment: '状态:  0=禁用 1=开启	',
+  })
+  status: number;
+
+  @Column({
+    name: 'order_num',
+    type: 'int',
+    unsigned: true,
+    default: 0,
+    comment: '排序值',
+  })
+  orderNum: number;
+
+  @Column({ type: 'varchar', length: 200, default: '', comment: '备注' })
+  remark: string;
+}
