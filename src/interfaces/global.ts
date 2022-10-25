51dcf9008efe71a3bd973120798d3a545e26ca0c
小小源逼 <qa894178522@qq.com>
2022-10-25 14:44:05 +0800

    feat: add tree util

--- src/interfaces/global.ts
+++ src/interfaces/global.ts
@@ -1,6 +1,17 @@
 import type { PaginationInfo, PaginationResData } from './response';
 
 declare global {
+  interface Fn<T = any, R = T> {
+    (...arg: T[]): R;
+  }
+
+  type NonNull<T> = T extends null | undefined ? never : T;
+  type Arrayable<T> = T | T[];
+  type Nullable<T> = T | null;
+  type Nilable<T> = T | null | undefined;
+  type Recordable<T = any> = Record<string, T>;
+
+  // Array polyfill
   interface Array<T> {
     /**
      * Generate pagination result
